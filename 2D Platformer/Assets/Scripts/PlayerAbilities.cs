14c64dad7816ddb95ccbfc4461abef9eb36fc26f
efekantgn <56132486+efekantgn@users.noreply.github.com>
2021-03-25 18:59:05 +0300

    Yenilikler eklendi.
    Fırlatma mekaniği eklendi.
    Bırakma mekaniği çıkarıldı.
    Karakteri takip eden kamera eklendi.(Cinemachine paketi ile eklendi.)

--- 2D Platformer/Assets/Scripts/PlayerAbilities.cs
+++ 2D Platformer/Assets/Scripts/PlayerAbilities.cs
@@ -8,7 +8,7 @@ public class PlayerAbilities : MonoBehaviour
     public Transform grabDetect;
     public Transform boxHolder;
     public float rayDist;
-    
+    public float throwSpeed;
 
     // Update is called once per frame
     void Update()
@@ -19,9 +19,11 @@ public class PlayerAbilities : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.F))
             {
+                
                 grabCheck.collider.gameObject.transform.parent = boxHolder;
                 grabCheck.collider.gameObject.transform.position = boxHolder.position;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwSpeed;
 
             }
             else
